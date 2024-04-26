701136aa0e885d4d05ce3d9fbbe4e577f18113f0
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-26 15:30:33 +0400

    1st task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,7 +9,7 @@ if len(sys.argv) == 2:
     res = requests.get(f"{url}users/{usr_id}/todos").json()
     res1 = requests.get(f"{url}users/{usr_id}").json()
     string = ""
-    usr_name = res1["name"]
+    usr_name = res1["username"]
     for i in res:
         string += '"{}","{}","{}","{}"\n'.format(usr_id, usr_name,
                                                  i["completed"], i["title"])
