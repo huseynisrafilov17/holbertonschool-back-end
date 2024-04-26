1160815c21f6a1553e5512474754357a7590970a
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-26 13:27:51 +0400

    0th task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,20 +4,19 @@ import requests
 import sys
 
 
-if __name__ == "__main__" and len(sys.argv) == 2:
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
-    r1 = requests.get(f"{url}users/{user_id}/todos")
-    r2 = requests.get(f"{url}users/{user_id}")
-    response1 = r1.json()
-    response2 = r2.json()
-    comp = 0
-    total = len(response1)
-    tasks = ""
+url = "https://jsonplaceholder.typicode.com/"
+user_id = sys.argv[1]
+r1 = requests.get(f"{url}users/{user_id}/todos")
+r2 = requests.get(f"{url}users/{user_id}")
+response1 = r1.json()
+response2 = r2.json()
+comp = 0
+total = len(response1)
+tasks = ""
 
-    for i in response1:
-        if i["completed"]:
-            comp += 1
-            tasks += f"\t {i['title']}\n"
-    print(f"Employee {response2['name']} is done with tasks({comp}/{total})")
-    print(tasks, end="")
+for i in response1:
+    if i["completed"]:
+        comp += 1
+        tasks += f"\t {i['title']}\n"
+print(f"Employee {response2['name']} is done with tasks({comp}/{total}):")
+print(tasks, end="")
