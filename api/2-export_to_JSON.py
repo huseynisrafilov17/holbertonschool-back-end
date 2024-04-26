f127e35f928f3c1e7adfd623a88aa0fd19053570
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-26 15:38:48 +0400

    2nd task

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""My API usage"""
+import requests
+import sys
+import json
+
+if len(sys.argv) == 2:
+    url = "https://jsonplaceholder.typicode.com/"
+    usr_id = sys.argv[1]
+    dictionary = {f"{usr_id}": []}
+    res = requests.get(f"{url}users/{usr_id}/todos").json()
+    res1 = requests.get(f"{url}users/{usr_id}").json()
+    usr_name = res1["username"]
+    string = ""
+    for i in res:
+        new_dict = {}
+        new_dict["task"] = i["title"]
+        new_dict["completed"] = i["completed"]
+        new_dict["username"] = usr_name
+        dictionary[f"{usr_id}"].append(new_dict)
+
+    with open(f"{usr_id}.json", "w") as f:
+        f.write(json.dumps(dictionary))
