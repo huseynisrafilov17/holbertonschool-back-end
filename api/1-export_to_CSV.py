d5b3bafdc9c1d8e448941754ef7f257625dd68d3
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-26 13:56:45 +0400

    2nd task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,13 +5,14 @@ import sys
 
 if len(sys.argv) == 2:
     url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
-    res = requests.get(f"{url}users/{user_id}/todos").json()
-    res1 = requests.get(f"{url}users/{user_id}").json()
+    usr_id = sys.argv[1]
+    res = requests.get(f"{url}users/{usr_id}/todos").json()
+    res1 = requests.get(f"{url}users/{usr_id}").json()
     string = ""
-    user_name = res1["name"]
+    usr_name = res1["name"]
     for i in res:
-        string += f'"{user_id}","{user_name}","{i["completed"]}","{i["title"]}"\n'
+        string += f'''"{usr_id}","{usr_name}","{i["completed"]}",
+                   "{i["title"]}"\n'''
 
-    with open(f"{user_id}.csv", "w") as f:
+    with open(f"{usr_id}.csv", "w") as f:
         f.write(string)
